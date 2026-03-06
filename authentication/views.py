4bafb5e75c9acac9cffdb45680537481dc6556ac
Aurélien <aurelien.lebray@gmail.com>
2026-03-06 18:21:00 +0100

    Added sign in view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+
+from authentication.forms import UserCreationForm
+
 
 # Create your views here.
+class SigninView(FormView):
+    form_class = UserCreationForm
+    template_name = "authentication/sign-in.html"
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('connexion/', authentication.forms)
+    path('inscription/', views.SigninView.as_view(), name="sign-in")
 ]
