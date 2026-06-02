93c5786369d9d964402ae2757232883e0b303bca
Aurélien <aurelien.lebray@gmail.com>
2026-06-02 12:40:20 +0200

    Lesson: 02/06/2026

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from social_network.views import FeedView, TicketCreateView, ReviewCreateView, TicketAndReviewCreateView, TicketUpdateView, TicketDeleteView
+from social_network.views import (
+    FeedView,
+    HomeView,
+    TicketCreateView,
+    ReviewCreateView,
+    TicketAndReviewCreateView,
+    TicketUpdateView,
+    ReviewUpdateView,
+    TicketDeleteView,
+    ReviewDeleteView,
+)
 
 # A titre perso :
 # On remplie le path en mettant l'url que l'on veut avoir,
@@ -9,12 +19,13 @@ from social_network.views import FeedView, TicketCreateView, ReviewCreateView, T
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name="feed"),
+    path('home/', HomeView.as_view(), name="home"),
     path('create-ticket/', TicketCreateView.as_view(), name="create_ticket"),
     path('create-review/<int:ticket_id>/', ReviewCreateView.as_view(), name="create_review"),
     path('create-ticket-and-review/', TicketAndReviewCreateView.as_view(),name="create_ticket_and_review"),
     path('update-ticket/<int:pk>/', TicketUpdateView.as_view(), name="update_ticket"),
     path('delete-ticket/<int:pk>/', TicketDeleteView.as_view(), name="delete_ticket"),
-    path('update-review/<int:pk>/', TicketUpdateView.as_view(), name="update_review"),
-    path('delete-review/<int:pk>/', TicketDeleteView.as_view(), name="delete_review")
+    path('update-review/<int:pk>/', ReviewUpdateView.as_view(), name="update_review"),
+    path('delete-review/<int:pk>/', ReviewDeleteView.as_view(), name="delete_review"),
 
 ]
--- social_network/views.py
+++ social_network/views.py
@@ -10,6 +10,43 @@ from social_network.forms import TicketForm, ReviewForm
 from social_network.models import Ticket, Review
 
 
+class HomeView(LoginRequiredMixin, TemplateView):
+    """
+    Vue pour tous les posts des personnes suivies et les nôtres
+    """
+    template_name = "social_network/home.html"
+
+    def get_users_viewable_reviews(self):
+        current_user = self.request.user
+        followed_users = current_user.following_users.values_list('followed_user', flat=True)
+        return (
+            Review.objects.filter(user=current_user) |
+            Review.objects.filter(user__in=followed_users) |
+            Review.objects.filter(ticket__user=current_user)
+        ).distinct()
+
+    def get_users_viewable_tickets(self):
+        current_user = self.request.user
+        followed_users = current_user.following_users.values_list('followed_user', flat=True)
+        return (
+            Ticket.objects.filter(user=current_user) |
+            Ticket.objects.filter(user__in=followed_users)
+        ).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = self.get_users_viewable_reviews()
+        reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+        tickets = self.get_users_viewable_tickets()
+        tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+        posts = sorted(
+            chain(reviews, tickets),
+            key=lambda post: post.time_created,
+            reverse=True
+        )
+        context["posts"] = posts
+        return context
+
 class FeedView(LoginRequiredMixin, TemplateView):
     template_name = "social_network/feed.html"
 
